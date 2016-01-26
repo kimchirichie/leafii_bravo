99da0183d811d478790ff19c17e9ad7ffb97bb26
Talent Zeng <talent.z@hotmail.com>
2016-01-25 19:43:06 -0500

    Scarlett Minshull

---
+++ apartment/scminshull/javascript/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $("#scarsinOpen").click(function(){
+    $("#scarsinFeedback").slideToggle("slow");
+  });
+
+  $("#blueOpen").click(function(){
+    $("#blueFeedback").slideToggle("slow");
+  });
+
+});
+
