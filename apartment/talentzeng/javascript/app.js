cdf0d2c7f1374554e8e3b1591c68c6e3492dc0bc
ttrzeng <talent.z@Hotmail.com>
2016-11-07 15:37:31 -0500

    getting rid of the hashtag, i hope this works

--- apartment/talentzeng/javascript/app.js
+++ apartment/talentzeng/javascript/app.js
@@ -7,49 +7,49 @@ TalentPage.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
 
 		$stateProvider
 			.state('blogPosts', {
-				url: '/',
+				url: '/talentzeng/',
 				templateUrl: 'posts.html'
 			})
 
 			.state('profile', {
-				url: '/profile',
+				url: '/talentzeng/profile',
 				templateUrl: 'profile.html'
 			})
 
 			.state('experience', {
-				url: '/experience',
+				url: '/talentzeng/experience',
 				templateUrl: 'experience.html'
 			})
 
 			.state('projects', {
-				url: '/projects',
+				url: '/talentzeng/projects',
 				templateUrl: 'projects.html'
 			})
 
 			.state('blog4', {
-				url: '/11-06-2016',
+				url: '/talentzeng/11-06-2016',
 				templateUrl: 'blogposts/11-06-2016.html'
 			})
 
 			.state('blog3', {
-				url: '/09-06-2016',
+				url: '/talentzeng/09-06-2016',
 				templateUrl: 'blogposts/09-06-2016.html'
 			})
 
 			.state('blog2', {
-				url: '/06-19-2016',
+				url: '/talentzeng/06-19-2016',
 				templateUrl: 'blogposts/06-19-2016.html'
 			})
 
 			.state('blog1', {
-				url: '/05-19-2016',
+				url: '/talentzeng/05-19-2016',
 				templateUrl: 'blogposts/05-19-2016.html'
 		});
 
-		// $locationProvider.html5Mode({
-		// 	enabled: true,
-		// 	requireBase: false
-		// });
+		$locationProvider.html5Mode({
+			enabled: true,
+			requireBase: false
+		});
 	}
 ]);
 
