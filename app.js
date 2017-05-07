d747ae4dd0b4600b080eed8a0aaef48f7d50b96d
brainfogz <sinr0202@gmail.com>
2017-05-06 20:34:53 -0400

    removed unwanted filed and folders

--- app.js
+++ app.js
@@ -5,22 +5,13 @@ var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var session = require("express-session");
-// var passport = require("passport");
 var flash = require("connect-flash");
 var cors = require('cors');
-
-// DATABASE MODEL
-// var User = require("./models/user");
-
-// ROUTER
-// var auth = require("./router/auth");
-// var profile = require("./router/profile");
 
 // SERVER OPERATION
 var app = express();
 
 // view engine setup
-// app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
@@ -31,24 +22,7 @@ app.use(cors());
 
 // PASSPORT CONFIG
 app.use(session({ secret: "87dfyas9fy78234y82f25g35tg" })); // session secret
-// app.use(passport.initialize());
-// app.use(passport.session()); // persistent login sessions
 app.use(flash()); // flash message
-// passport.serializeUser(function(user, done) {
-//   console.log("Serialize User: " + user.id);
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser(function(id, done) {
-//   console.log("Deserialize User: " + id);
-//   User.findById(id).then(function(user) {
-//     done(null, user);
-//   });
-// });
-
-// LOG TRAFFIC
-// app.use("/auth", auth);
-// app.use("/profile", profile);
 
 // PUBLIC ASSETS BEFORE PRIVATE ASSETS
 app.use(express.static(path.join(__dirname, "bower_components")));
