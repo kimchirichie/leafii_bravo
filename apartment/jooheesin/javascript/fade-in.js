92083f72985cb393a162f6a2ecb1504a18823150
brainfogz <sinr0202@gmail.com>
2017-10-26 23:42:03 -0400

    rid of old stuff a and put clickmd landing page

--- apartment/jooheesin/javascript/fade-in.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	setTimeout(function(){
-		$('.content').show();
-		$('#first').css('visibility','visible').hide().fadeIn('slow');
-		setTimeout(function(){
-			$('#second').css('visibility','visible').hide().fadeIn('slow');
-		},1000);
-	},1000);
-});
