60ec0a12f5b5dbb2b585d36d033f0e2dc94e39e6
Richard Sin <sinr0202@gmail.com>
2018-07-12 14:13:00 -0700

    added api

---
+++ apartment/api/controller.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/*', function(req, res, next) {
+	console.log(req.hostname)
+	console.log(req.baseUrl)
+	console.log(req.originalUrl)
+	console.log(req.path)
+	console.log(req.method)
+	console.log(req.body)
+	console.log(req.query)
+	console.log(req.param)
+	res.send({
+		hostname:req.hostname,
+		baseUrl:req.baseUrl,
+		originalUrl:req.originalUrl,
+		path:req.path,
+		method:req.method,
+		body:req.body,
+		query:req.query,
+		param:req.param,
+	});
+});
+
+module.exports = router;
