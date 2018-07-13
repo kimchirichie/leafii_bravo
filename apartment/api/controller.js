67d1557dce4a67f23ffff9c73ed87527789d07ba
Richard Sin <sinr0202@gmail.com>
2018-07-13 11:43:15 -0700

    changed the get to an all

--- apartment/api/controller.js
+++ apartment/api/controller.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/*', function(req, res, next) {
+router.all('/*', function(req, res, next) {
 	console.log(req.hostname)
 	console.log(req.baseUrl)
 	console.log(req.originalUrl)
